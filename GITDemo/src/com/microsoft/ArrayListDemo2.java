8bd5d84f6c959951602e45234db805018909309b
unknown <prafulteltumbade@9823.com>
2024-01-29 19:35:40 +0530

    added print line in arraylist demo2 file

--- GITDemo/src/com/microsoft/ArrayListDemo2.java
+++ GITDemo/src/com/microsoft/ArrayListDemo2.java
@@ -1,5 +1,19 @@
 package com.microsoft;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class ArrayListDemo2 {
 
+	public static void main(String[] args) {
+		
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(30);
+		list.add(50);
+		list.add(70);
+		list.add(90);
+		
+		System.out.println(list);
+		System.out.println("List number data is>>>>>");
+	}
 }
