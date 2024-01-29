809c012c95eaad880a7dbd5c542d631c2ffe77ed
unknown <prafulteltumbade@9823.com>
2024-01-29 19:03:06 +0530

    added arraylist code for the city

---
+++ GITDemo/src/com/microsoft/ArrayListDemo1.java
@@ -0,0 +1,17 @@
+package com.microsoft;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo1 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> arrayList = new ArrayList<String>();
+		arrayList.add("pune");
+		arrayList.add("mumbai");
+		arrayList.add("bangalore");
+		arrayList.add("delhi");
+		
+		System.out.println(arrayList);
+	}
+}
