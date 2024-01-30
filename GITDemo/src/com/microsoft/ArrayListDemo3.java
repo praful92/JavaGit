d3ff6a899cd3cedb71652d1be87d76dc565b5327
praful <prafultells@gmail.com>
2024-01-30 12:29:01 +0530

    added arraylist code for number and city

---
+++ GITDemo/src/com/microsoft/ArrayListDemo3.java
@@ -0,0 +1,17 @@
+package com.microsoft;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo3 {
+
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		al.add(10);
+		al.add(20);
+		al.add("pune");
+		al.add("mumbai");
+		System.out.println(al);
+		
+	}
+}
