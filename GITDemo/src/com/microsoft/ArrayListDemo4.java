0268b3b72e539e6ad4ef7b792684bfbf3a7436b5
praful <prafultell@gmail.com>
2024-02-02 15:10:24 +0530

    praful

---
+++ GITDemo/src/com/microsoft/ArrayListDemo4.java
@@ -0,0 +1,17 @@
+package com.microsoft;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo4 {
+
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		al.add(100);
+		al.add(200);
+		al.add("delhi");
+		al.add("hydrabad");
+		System.out.println(al);
+		
+	}
+}
